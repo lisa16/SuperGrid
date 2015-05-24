db17460f518cfcb2fdf198c7cc06a3f17cf7c2d8
Lisa W <miss.lisa19@hotmail.com>
2015-05-24 10:20:33 -0700

    changing color sync

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -4,10 +4,12 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour {
     public static GameObject currentGameObject;
+
+    private NetworkView _networkView;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        _networkView = this.GetComponent<NetworkView>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,11 +18,15 @@ public class CharacterSelection : MonoBehaviour {
 	}
 
 
+    [RPC]
     public void PlayerChangeColor()
     {
-        GameObject RPCBroadcast = GameObject.FindGameObjectWithTag("RPCBroadcast");
-        RPCBroadcast.SendMessage("PlayerReadyBroadcast");
+        //GameObject RPCBroadcast = GameObject.FindGameObjectWithTag("RPCBroadcast");
+        //RPCBroadcast.SendMessage("PlayerReadyBroadcast");
         Image currImg = currentGameObject.GetComponentInChildren<Image>();
         currImg.color = Color.red;
+
+        if (_networkView.isMine)
+            _networkView.RPC("PlayerChangeColor", RPCMode.OthersBuffered);
     }
 }
