3eb2ea1b711b067feeab7cbcabeba4ca18f577d0
ahung93 <ahung93@gmail.com>
2015-05-24 13:27:59 -0700

    Adding list of questions

--- Assets/Scripts/ListOfQuestions.cs
+++ Assets/Scripts/ListOfQuestions.cs
@@ -2,20 +2,18 @@
 using System.Collections;
 
 public class ListOfQuestions {
-
 	public static QuestionClass [] questionList;
-
 	public static void setList(){
 		questionList = new QuestionClass[]{
-			new QuestionClass ("This is Question 1", "Answer 1A", "Answer 1B", "Answer 1C", "Answer 1D", "D"),
-			new QuestionClass ("This is Question 2", "Answer 2A", "Answer 2B", "Answer 2C", "Answer 2D", "A"),
-			new QuestionClass ("This is Question 3", "Answer 3A", "Answer 3B", "Answer 3C", "Answer 3D", "D"),
-			new QuestionClass ("This is Question 4", "Answer 4A", "Answer 4B", "Answer 4C", "Answer 4D", "B"),
-			new QuestionClass ("This is Question 5", "Answer 5A", "Answer 5B", "Answer 5C", "Answer 5D", "C"),
-			new QuestionClass ("This is Question 6", "Answer 6A", "Answer 6B", "Answer 6C", "Answer 6D", "A"),
-			new QuestionClass ("This is Question 7", "Answer 7A", "Answer 7B", "Answer 7C", "Answer 7D", "A"),
-			new QuestionClass ("This is Question 8", "Answer 8A", "Answer 8B", "Answer 8C", "Answer 8D", "B"),
-			new QuestionClass ("This is Question 9", "Answer 9A", "Answer 9B", "Answer 9C", "Answer 9D", "D"),
-			new QuestionClass ("This is Question 10", "Answer 10A", "Answer 10B", "Answer 10C", "Answer 10D", "C")};
+			new QuestionClass ("Which resistor would produce 50mA of current with 50V battery in a series circuit?", "Answer 100Ω resistor", "Answer 500Ω resistor", "Answer 10Ω resistor", "Answer 1000Ω resistor", "D"),
+			new QuestionClass ("What is the voltage of a series circuit with 50Ω resistor with 25A current?", "Answer 1250V", "Answer 2V", "Answer 0.5V", "Answer 125V", "A"),
+			new QuestionClass ("Which resistor would produce 7.5A of current with 75V battery in a series circuit?", "Answer 75Ω resistor", "Answer 750Ω resistor", "Answer 0.1Ω resistor", "Answer 10Ω resistor", "D"),
+			new QuestionClass ("What is the current of a series ciruit with 561 540V battery and 420Ω resistor", "Answer 1320A", "Answer 1337A", "Answer 523 402mA", "Answer 9999A", "B"),
+			new QuestionClass ("What is the current of a series ciruit with 9999V battery and 9Ω resistor", "Answer 11mA", "Answer 99A", "Answer 1111A", "Answer 10A", "C"),
+			new QuestionClass ("What is the voltage of a series circuit with 1kΩ resistor with 123A current?", "Answer 123kV", "Answer 1230V", "Answer 1kV", "Answer 1400V", "A"),
+			new QuestionClass ("Which resistor would produce 1mA of current with 1000V battery in a series circuit?", "Answer 1 000 000Ω resistor", "Answer 0.0001Ω resistor", "Answer 1010Ω resistor", "Answer 1000Ω resistor", "A"),
+			new QuestionClass ("What is the voltage of a series circuit with 7Ω resistor with 48A current?", "Answer 286mV", "Answer 336V", "Answer 286V", "Answer 316V", "B"),
+			new QuestionClass ("What is the current of a series ciruit with 10 101 010V battery and 1010Ω resistor", "Answer 1111A", "Answer 10 000A", "Answer 1010mA", "Answer 10 001A", "D"),
+			new QuestionClass ("Which resistor would produce 1A of current with 1V battery in a series circuit?", "Answer 100Ω resistor", "Answer 10Ω resistor", "Answer 1Ω resistor", "Answer 1000Ω resistor", "C")};
 	}
 }
