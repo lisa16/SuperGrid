70baec08fc53c0c1763fe9c22fe56baf4b1d4c80
koreanJESUS <yeahan014@gmail.com>
2015-05-24 00:41:31 -0700

    buttonclicker

---
+++ Assets/Scripts/ButtonClickListener.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ButtonClickListener : MonoBehaviour {
+	[HideInInspector]
+	private string CurrentPlayer = "RoomPlayer4";
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SlotClicked() {
+		Image img = this.gameObject.GetComponent<Image> ();
+		if (img.color != Color.white) {
+			return;
+		} else {
+			CurrentPlayer = this.gameObject.tag;
+			img.color = Color.green;
+		}
+	}
+
+	public void ButtonClicked() {
+		if (CurrentPlayer == "") {
+			return;
+		}
+
+		GameObject PlayerRoom = GameObject.FindGameObjectWithTag(CurrentPlayer) as GameObject;
+		Image img = PlayerRoom.GetComponent<Image> ();
+		img.color = Color.red;
+	}
+}
---
+++ Assets/UnityVS/InGameLobby/InGameLobby.cs
@@ -0,0 +1,18 @@
+﻿///attached to the canvas
+
+using UnityEngine;
+using System.Collections;
+
+public class InGameLobby : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	}
