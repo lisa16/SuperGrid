10ce8ce1c8121e327e60cf2973126f82e16fbb42
Lisa W <miss.lisa19@hotmail.com>
2015-05-24 14:05:01 -0700

    make platform and hospital

--- Assets/GameSettings.cs
+++ Assets/GameSettings.cs
@@ -9,12 +9,18 @@ public class GameSettings : MonoBehaviour {
     private const int scoreXOffset = 5;
     private static Vector3 initTransform;
 
+    public static GameObject litPlatform;
+    public static GameObject litHospital;
+
+    private static Vector3 initPlatformTransform;
+
 	// Use this for initialization
 	void Start () {
         myPlayerAnim = Network.Instantiate(playerAnimList[GetInputText.characterAssignedIndex], playerAnimList[GetInputText.characterAssignedIndex].transform.position, Quaternion.identity, 0) as GameObject;
         initTransform = myPlayerAnim.transform.position;
         GameObject _cam = GameObject.FindGameObjectWithTag("MainCamera");
         _cam.transform.SetParent(myPlayerAnim.transform);
+        initPlatformTransform = litPlatform.transform.position;
     }
 	
 	// Update is called once per frame
@@ -26,5 +32,9 @@ public class GameSettings : MonoBehaviour {
     {
         currentScore++;
         myPlayerAnim.transform.position = new Vector2(initTransform.x + scoreXOffset * currentScore, initTransform.y);
+        Instantiate(litPlatform, new Vector2(initPlatformTransform.x + scoreXOffset * currentScore, initPlatformTransform.y), Quaternion.identity);
+        if(currentScore == 10)
+            Instantiate(litHospital);
+
     }
 }
