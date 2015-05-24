655741858a33b6368844eb72770a12c861b5db06
koreanJESUS <yeahan014@gmail.com>
2015-05-24 11:36:38 -0700

    Added tutorial layer. going to change the tutorial pages later once we get it

---
+++ Assets/Scripts/TutorialPages.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialPages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TutorialPage1Ok()
+    {
+        GameObject TutorialPage1 = GameObject.FindGameObjectWithTag("TutorialPage1") as GameObject;
+        Destroy(TutorialPage1);
+        GameObject TutorialPage1Button = GameObject.FindGameObjectWithTag("TutorialPage1Button") as GameObject;
+        Destroy(TutorialPage1Button);
+    }
+
+    public void TutorialPage2Ok()
+    {
+        GameObject TutorialPage2 = GameObject.FindGameObjectWithTag("TutorialPage2") as GameObject;
+        Destroy(TutorialPage2);
+        GameObject TutorialPage2Button = GameObject.FindGameObjectWithTag("TutorialPage2Button") as GameObject;
+        Destroy(TutorialPage2Button);
+        Application.LoadLevel("StartScene");
+    }
+
+}
